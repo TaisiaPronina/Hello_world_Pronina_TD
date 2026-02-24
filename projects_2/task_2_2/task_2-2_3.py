54e477387b7550bbe5f945c99541481df90bb3ce
Пронина Таисия Дмитриевна <tayapronina@mail.ru>
2026-02-24 17:15:58 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+device_name = "Компутер"
+device_inventory_number = "52"
+device_status = "Исправен"
+number_of_device = "1"
+
+print (f"Название прибора\t{device_name}\nНомер прибора\t{device_inventory_number}\nСостояние\t{device_status}\nКоличество\t{number_of_device}")
